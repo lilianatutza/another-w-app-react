482824c0b0395ea908358941a547894dfb7cee26
Liliana Tuta <lilianatutza@gmail.com>
2024-12-12 11:51:00 +0000

    Added the git hub Url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">My weather app</div>
+    <div className="App">
+      <h1>My weather app</h1>
+      <footer>
+        This project is codded by <a
+          href="https://github.com/dashboard"
+          target="_blank"rel='noreferrer'>
+          Liliana Tuta</a>,{" "}is open sourced on <a
+          href="https://github.com/lilianatutza/another-w-app-react"
+          target="_blank"rel='noreferrer'>  GitHub
+         </a> and is hosted on
+        <a
+          href="https://splendorous-platypus-539b6e.netlify.app/"
+          target="_blank"rel='noreferrer'> Netifly.
+          </a>
+      </footer>
+    </div>
   );
 }
