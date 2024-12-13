e2668c8d4d6851b8be56f0117c00ec009e180e26
Liliana Tuta <lilianatutza@gmail.com>
2024-12-13 17:22:33 +0000

    Created a component for date

---
+++ src/FormattedDate.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function FormattedDate(props) {
+  return <div>
+  "Friday 17:09"
+  </div>
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate"
 import "./Weather.css";
 import "./App.css";
 
@@ -9,7 +10,7 @@ export default function Weather(props) {
     console.log(response.data);
     setWeatherData({
       ready:true,
-      date:"Sunday 09:00",
+      date:new Date(response.data.dt*1000),
       city: response.data.name,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
@@ -45,7 +46,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
