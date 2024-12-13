577f3b474c8f975f0ea8fa1e1889f1e34dedbd3b
Liliana Tuta <lilianatutza@gmail.com>
2024-12-13 23:28:44 +0000

    position of temperature adjust

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,16 +15,13 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="float">
+         
+            <div className="d-flex">
               <WeatherIcon code={props.data.icon} />
 
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
-          </div>
+          
         </div>
 
         <div className="col-6">
