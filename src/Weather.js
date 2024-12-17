e68022c1dc5fdca304ab74d283c3253f42157da5
Liliana Tuta <lilianatutza@gmail.com>
2024-12-17 15:17:49 +0000

    bugs present, not sure

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
+import WeatherForecast from "./WeatherForecastDay";
 
 import "./Weather.css";
 import "./App.css";
